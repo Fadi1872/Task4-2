a1b77ebe4dae9fac46a6de379c681a714a308a7d
Freddy18 <fadinoumih18@gmail.com>
2023-09-08 00:55:40 +0300

    All Sections

---
+++ assete/js/app.js
@@ -0,0 +1,18 @@
+function resize (){
+    var VPwidath = screen.width;
+    if(VPwidath < 768){
+        let news1 = document.getElementById('news1');
+        news1.src = "./assete/img/news1H.jpg";
+        let news2 = document.getElementById('news2');
+        news2.src = "./assete/img/news2H.jpg";
+    }
+    else if(VPwidath >= 768){
+        let news1 = document.getElementById('news1');
+        news1.src = "./assete/img/news1V.jpg";
+        let news2 = document.getElementById('news2');
+        news2.src = "./assete/img/news2V.jpg";
+    }
+};
+
+window.onresize = resize;
+
